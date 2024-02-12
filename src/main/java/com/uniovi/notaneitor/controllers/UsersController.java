b1d78a60f265c1b3feca814f28804a6eda1e0f38
dono <uo288787@uniovi.es>
2024-02-12 15:30:57 +0100

    SDI-2324-509-3.5-Validación de datos en el servidor.

--- src/main/java/com/uniovi/notaneitor/controllers/UsersController.java
+++ src/main/java/com/uniovi/notaneitor/controllers/UsersController.java
@@ -1,10 +1,13 @@
 package com.uniovi.notaneitor.controllers;
 
 import com.uniovi.notaneitor.services.SecurityService;
+import com.uniovi.notaneitor.validators.SignUpFormValidator;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import com.uniovi.notaneitor.entities.*;
 import com.uniovi.notaneitor.services.UsersService;
@@ -14,10 +17,12 @@ public class UsersController {
 
     private final UsersService usersService;
     private final SecurityService securityService;
+    private final SignUpFormValidator signUpFormValidator;
 
-    public UsersController(UsersService usersService, SecurityService securityService) {
+    public UsersController(UsersService usersService, SecurityService securityService, SignUpFormValidator signUpFormValidator) {
         this.usersService = usersService;
         this.securityService = securityService;
+        this.signUpFormValidator = signUpFormValidator;
     }
 
     @RequestMapping("/user/list")
@@ -63,8 +68,19 @@ public class UsersController {
 
     // USER REGISTER
 
+    @RequestMapping(value = "/signup", method = RequestMethod.GET)
+    public String signup(Model model) {
+        model.addAttribute("user", new User());
+        return "signup";
+    }
+
     @RequestMapping(value = "/signup", method = RequestMethod.POST)
-    public String signup(@ModelAttribute("user") User user, Model model) {
+    public String signup(@Validated User user, BindingResult result) {
+        signUpFormValidator.validate(user, result);
+        if (result.hasErrors()) {
+            return "signup";
+        }
+
         usersService.addUser(user);
         securityService.autoLogin(user.getDni(), user.getPasswordConfirm());
         return "redirect:home";
